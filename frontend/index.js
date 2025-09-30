808ab83642feaaf82cfe909f649f72a664714f70
tharshihecker <tharshikan287@gmail.com>
2025-09-30 15:59:04 +0530

    Initial commit - pushing full project

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-// Optional JS: prevent multiple submissions and show loader
-document
-  .getElementById("personalityForm")
-  .addEventListener("submit", function () {
-    const btn = this.querySelector('button[type="submit"]');
-    btn.innerText = "Predicting...";
-    btn.disabled = true;
-  });
